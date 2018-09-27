1f4c750e787f2aca76b544b8473ad4561940357e
KristianWB <42993999+KristianWB@users.noreply.github.com>
2018-09-27 07:07:09 +0200

    Initial commit

---
+++ src/HealthApplication_BMI.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class HealthApplication_BMI {
+    public static void main(String[] args)   {
+        Scanner input = new Scanner(System.in);
+
+        //prompt the user to enter weight in pounds
+        System.out.print("Enter weight in pounds: ");
+        double weight = input.nextDouble();
+
+        // Prompt the user to enter height in inches
+        System.out.println("Enter height in feet and inches: ");
+        System.out.print("Enter feet: ");
+        double feet = input.nextDouble();
+        System.out.print("Enter inches: ");
+        double inches = input.nextDouble();
+
+        final double KILOGRAMS_PER_POUND = 0.45359237; //Constant
+        final double METERS_PER_INCH = 0.0254; //Constant
+
+        //Compute BMI
+        double weightInKilograms = weight * KILOGRAMS_PER_POUND;
+        double heightInMeters = (feet * 12 + inches) * METERS_PER_INCH;
+        double bmi = weightInKilograms / (heightInMeters * heightInMeters);
+
+        // Display results
+        System.out.println("BMI is " + bmi);
+        if (bmi < 18.5)
+            System.out.println("Underweight");
+        else if (bmi < 25)
+            System.out.println("Normal");
+        else if (bmi < 30)
+            System.out.println("Overweight");
+        else
+            System.out.println("Obese");
+    }
+}
